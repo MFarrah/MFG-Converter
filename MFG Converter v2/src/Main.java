9f684cbfa9488e8d917050efb081cfc17a9101e7
Michael Farrah Girgis <149530935+Tapelove@users.noreply.github.com>
2023-11-14 20:54:24 +0100

    Update Main.java
    Het geeft mij nogsteeds geen uitkomst omdat ik chosenConversionRates1 & 2 binnen de loop heb gedeclared. Dit heb ik gecorrigeerd door ze buiten de loop te zetten maar kan vervolgens geen ref maken door (double) weg te halen in de loops. error messages.

--- MFG Converter v2/src/Main.java
+++ MFG Converter v2/src/Main.java
@@ -8,46 +8,106 @@ public class Main {
         Scanner userInput = new Scanner(System.in);
 
         //String-Array met valuta benamingen + een double-Array met conversie ratios
-                String[] currencies = {"Euro", "US Dollar", "GB Pond", "Bitcoin", "Ethereum", "Solana"};
-                double[][] conversionRates = {
-                        {1.18, 0.85, 0.86, 0.000025, 0.035, 15.88}, // EUR
-                        {0.85, 1.0, 0.73, 0.000021, 0.03, 13.5}, // USD
-                        {0.86, 1.37, 1.0, 0.000032, 0.044, 20.76}, // GBP
-                        {54000, 62000, 31000, 1.0, 22, 1050000}, // BTC
-                        {3500, 4000, 3000, 0.05, 1.0, 500}, // ETH
-                        {35, 40, 30, 0.000037, 0.2, 1.0} // SOL
+        String[] currencies = {"Euro", "US Dollar", "GB Pond", "Bitcoin", "Ethereum", "Solana"};
+        double[][] conversionRates = {
+                {1.0, 0.85, 0.86, 0.000025, 0.035, 15.88}, // EUR
+                {0.85, 1.0, 0.73, 0.000021, 0.03, 13.5}, // USD
+                {0.86, 1.37, 1.0, 0.000032, 0.044, 20.76}, // GBP
+                {54000, 62000, 31000, 1.0, 22, 1050000}, // BTC
+                {3500, 4000, 3000, 0.05, 1.0, 500}, // ETH
+                {35, 40, 30, 0.000037, 0.2, 1.0} // SOL
         };
 
-            //vraag : Welke valuta wilt u omrekenen ?
+        double chosenAmount = 0.0;
+        double[] chosenConversionrates = null;
+        double[] chosenConversionrates2 = null;
 
-            System.out.println("Welke valuta wilt u omrekenen ?");
+        //vraag : Welke valuta wilt u omrekenen ?
 
-            //begin van buitenste loop
+        System.out.println("Vanaf welke valuta wilt u omrekenen ?");
+
+        //begin van buitenste loop
 
         while (true) {
-
-
-                //for loop om een valuta te selecteren
-                for (int i = 0; i < currencies.length; i++) {
+            // for loop om een valuta te selecteren
+            for (int i = 0; i < currencies.length; i++) {
                 System.out.println((i + 1) + ". " + currencies[i]);
+            }
+
+            // Scanner labelen met int variable om een waarde aan te kunnen koppelen
+            int userChoice;
+
+            try {
+                userChoice = userInput.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Geen geldige invoer. Voer aub een geldig getal in.");
+                userInput.next(); // Clear the invalid input
+                continue;
+            }
+
+            // interne loop voor ongeldige invoer
+            if (userChoice < 1 || userChoice > currencies.length) {
+                System.out.println("Geen geldige invoer. Selecteer a.u.b. een valuta (1-6)..");
+                continue;
+
+
+            }
+            if (userChoice == currencies.length) {
+                double[] chosenConversionRates = conversionRates[userChoice-1];
+                userInput.nextInt();
+                break;
+            }
+//In de volgende loop word er gevraagd naar welke valuta er omgerekend kan worden
+
+            while (true) {
+                // for loop om een valuta te selecteren
+                System.out.println("Naar welke valuta wilt u omrekenen ?");
+
+
+                try {
+                    userChoice = userInput.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Geen geldige invoer. Voer aub een geldig getal in.");
+                    userInput.next(); // Clear the invalid input
+                    continue;
                 }
 
-                //Scanner labelen met int variable om een waarde aan te kunnen koppelen
-                int userChoice;
-                userChoice = userInput.nextInt();
-
-                //interne loop voor ongeldige invoer
-                if (userChoice < 1 || userChoice > currencies.length) + (InputMismatchException) {
-                System.out.println("Geen geldige invoer. Selecteer aub een valuta (1-6)..");
-                userInput.next();
-                continue;
-         }
+                // interne loop voor ongeldige invoer
+                if (userChoice < 1 || userChoice > currencies.length) {
+                    System.out.println("Geen geldige invoer. Selecteer a.u.b. een valuta (1-6)..");
+                    continue;
 
 
+                }
+                if (userChoice == currencies.length) {
+                    double[] chosenConversionRates2 = conversionRates[userChoice - 1];
+                    userInput.nextInt();
+                    break;
+                }
+
+//In deze loop zal er gevraagd worden om het bedrag dat omgerekend zal worden
+
+                while (true) {
+                    // for loop om een valuta te selecteren
+                    System.out.println("Wat is het bedrag ?");
 
 
-                    //vraag : Wat is het bedrag ?;
+                    try {
+                        userChoice = userInput.nextInt();
+                    } catch (InputMismatchException e) {
+                        System.out.println("Geen geldige invoer. Voer aub een geldig bedrag in (alleen cijfers)..");
+                        userInput.next(); // Clear the invalid input
+                        continue;
+                    }
 
+                        chosenAmount = userChoice;
+                        userInput.nextDouble();
+                        break;
+                }
 
-   }
+            }
+        }
+                double convertedAmount = (chosenAmount * chosenConversionrates[0]) / chosenConversionrates2[0];
+                System.out.println("userChoice = " + convertedAmount);
+    }
 }
